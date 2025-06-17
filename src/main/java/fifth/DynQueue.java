bebf93d0ad7740624edb38378b0fca8709577bdc
Egorka <trololo221@bk.ru>
2025-06-17 21:49:06 +0300

    Add Queue ATD

---
+++ src/main/java/fifth/DynQueue.java
@@ -0,0 +1,74 @@
+package fifth;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DynQueue<T> implements Queue<T> {
+
+    private Status peekStatus = Status.NIL;
+    private Status enqueueStatus = Status.NIL;
+    private Status dequeStatus = Status.NIL;
+
+
+    private int size;
+    private final LinkedList<T> storage;
+
+    public DynQueue() {
+        storage = new LinkedList<>();
+        size = 0;
+    }
+
+    @Override
+    public T peek() {
+        T res = null;
+
+        if (size == 0) {
+            peekStatus = Status.ERROR;
+        } else {
+            peekStatus = Status.OK;
+            res = storage.getLast();
+        }
+
+        return res;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void enqueue(T value) {
+        storage.addFirst(value);
+        enqueueStatus = Status.OK;
+        size++;
+    }
+
+    @Override
+    public void dequeue() {
+        if (size == 0) {
+            dequeStatus = Status.ERROR;
+        } else {
+            dequeStatus = Status.OK;
+            storage.removeLast();
+            size--;
+        }
+    }
+
+    public Status getPeekStatus() {
+        return peekStatus;
+    }
+
+    public Status getEnqueueStatus() {
+        return enqueueStatus;
+    }
+
+    public Status getDequeStatus() {
+        return dequeStatus;
+    }
+
+    public enum Status {
+        OK, ERROR, NIL
+    }
+}
