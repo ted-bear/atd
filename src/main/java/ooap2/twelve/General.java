2c0c896c5a1f5c85cb7b8fc541e7971ab9d0a9d9
e.d.toporkov <e.d.toporkov@tbank.ru>
2025-09-05 17:49:44 +0300

    Add three class hierarchy for vehicle

--- src/main/java/ooap2/twelve/General.java
+++ src/main/java/ooap2/twelve/General.java
@@ -1,36 +1,72 @@
 package ooap2.twelve;
 
-public class General implements Cloneable {
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-    @Override
-    protected final Object clone() throws CloneNotSupportedException {
-        return super.clone();
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+class General implements Serializable {
+
+    public <T> void deepCopy(T target) throws Exception {
+        try {
+            target = getCopy();
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
-    protected final General copy(Object other) throws CloneNotSupportedException {
-        other = clone();
-        return (General) other;
+    public <T> T deepClone() throws Exception {
+        try {
+            return getCopy();
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
     @Override
-    public final String toString() {
-        return super.toString();
-    }
-
-    @Override
-    public final boolean equals(Object obj) {
+    public boolean equals(Object obj) {
         return super.equals(obj);
     }
 
-    public final boolean isType(Class<?> type) {
-        return this.getClass().equals(type);
+    public <T> String serialize() throws JsonProcessingException {
+        var mapper = new ObjectMapper();
+        return mapper.writeValueAsString((T) this);
     }
 
+    public static <T> T deserialize(
+        String json,
+        Class<T> clazz) throws JsonProcessingException {
+        var mapper = new ObjectMapper();
+        return mapper.readValue(json, clazz);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @JsonIgnore
     public final Class<?> getType() {
         return this.getClass();
     }
 
-    public final void print() {
-        System.out.println(this);
+    private <T> T getCopy() throws Exception  {
+        try {
+            var byteArrayOutputStream = new ByteArrayOutputStream();
+            var objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject((T) this);
+            var bais = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+            var objectInputStream = new ObjectInputStream(bais);
+
+            return (T) objectInputStream.readObject();
+        }
+        catch (Exception e) {
+            throw e;
+        }
     }
 }
